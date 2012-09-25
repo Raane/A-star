9349dd6da5f9dccc527176ac7e5481d8337fbb6d
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-25 22:49:58 +0200

    Changing the use of state as it was wrong and commented alot of variables.

--- src/astar/SearchNode.java
+++ src/astar/SearchNode.java
@@ -2,25 +2,28 @@ package astar;
 
 import java.util.ArrayList;
 
-public class SearchNode implements Comparable{
+public class SearchNode implements Comparable{  // Implements comparable for sorting
+	private static final double ROUNDINGCONSTANT = 1000;
 	State state;
-	double f;
-	double g;
-	double h;
-	boolean status;
-	boolean solution;
-	SearchNode parrent;
-	ArrayList<SearchNode> children;
-	ArrayList<SearchNode> possParrent;
+	double f;							// This is the cost of getting to the node
+	double g;							// This is the estimated cost of getting from the node to the goal
+	double h;							// This is the sum of f & g
+	boolean open;						// This is the status (open/closed) of the node
+	boolean solution;					// This indicates if the node is a solution
+	SearchNode parrent;					// This indicates the best known parrent
+	ArrayList<SearchNode> children;		// This is all of the explorableChildren of node
+	ArrayList<SearchNode> possParrent;	// This is all of the possible parrents of the node
 	
 	public SearchNode() {
-		this.state = State.OPEN;
+		this.open = true;
 	}
-
+	
+	
+	
 	@Override
 	public int compareTo(Object anotherNode) {
 		if (!(anotherNode instanceof SearchNode))
 		      throw new ClassCastException("A node was expected.");
-		return (int)(this.f - ((SearchNode)anotherNode).f)*1000; //multiplying by 1000 to get more exact integer sorting
+		return (int)((this.f - ((SearchNode)anotherNode).f)*ROUNDINGCONSTANT); //multiplying by a rounding constant to get more exact integer sorting
 	}
 }
--- src/astar/State.java
+++ src/astar/State.java
@@ -1,5 +1,5 @@
 package astar;
 
 public enum State {
-	OPEN, CLOSED,
+	
 }
