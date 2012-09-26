d2df4069fb35fd06347de4f57c932adbfc555107
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 17:44:40 +0200

    Making the updateG into an updateParrent

--- src/astar/Astar.java
+++ src/astar/Astar.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Astar {
-	private static final State GOALSTATE = null; // This is where the goalstate should be coded in
+	private static final State GOALSTATE = null; 	// This is where the goalstate should be coded in
+	private static final State STARTINGSTATE = null; // This is where the startingstate should be coded in
 
 	private SearchNode goal;					// This store the goal
+	private SearchNode start;					// This is the startingNode
 	private ArrayList<SearchNode> agenda;		// This store the agenda
 	private ArrayList<SearchNode> closedNodes;	// This store the closed nodes
 	private ArrayList<SearchNode> visitedNodes;	// This store the visited nodes (including the closed one, but not the agenda)
@@ -24,12 +26,12 @@ public class Astar {
 		while(agenda.size()!=0) {				// This is the main agenda loop
 			Collections.sort(agenda);			// Sorting the agenda
 			SearchNode currentNode = agenda.remove(0);		// After sorting we can just look at the best looking candidate
-			if(currentNode.getState().equals(goal.getState())) {
+			if(currentNode.getState().equals(GOALSTATE)) {
 				return currentNode;								// return the first found solution
 			}
 			for(SearchNode closedNode:visitedNodes) {
 				if(currentNode.equals(closedNode) && !currentNode.equals(closedNode)) {	// Checks if there is another different node with the same state
-					closedNode.updateG(currentNode.getG());	// Lets the new identical node bubble up the graph
+					closedNode.addPossibleParrents(currentNode.getPossParrent());	// Lets the new identical node bubble up the graph
 					currentNode = closedNode;	// Sets the node we are working on to the new better and equal node
 					break;	// Exits as there should never be identical states in the nodes allready in the visited nodes
 				}
@@ -40,7 +42,7 @@ public class Astar {
 			if(visitedNodes.containsAll(currentNode.getChildren())) {
 				agenda.remove(currentNode);						// If all the children have been visited we will never have to return to the current node in the agenda loop
 			}
-			visitedNodes.add(currentNode); // TODO This will not work 
+			if(!currentNode.isVisited()) visitedNodes.add(currentNode);  // TODO remove this, it's not needed
 		}
 		return null;
 	}
--- src/astar/SearchNode.java
+++ src/astar/SearchNode.java
@@ -8,7 +8,7 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 	State state;						// This should contain the state of the problem
 	private double f;							// This is the cost of getting to the node
 	private double g;							// This is the estimated cost of getting from the node to the goal
-	private double h;							// This is the sum of f & g
+	private double h = -1;							// This is the sum of f & g
 	private boolean open;						// This is the status (open/closed) of the node
 	private boolean visited;
 	private boolean solution;					// This indicates if the node is a solution
@@ -16,10 +16,10 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 	private ArrayList<SearchNode> children;		// This is all of the explorableChildren of node
 	private ArrayList<SearchNode> possParrent;	// This is all of the possible parrents of the node
 	
-	public SearchNode(SearchNode parrent, SearchNode goal) {
+	public SearchNode(SearchNode parrent, State goalState) {
 		this.open = true;			// A fresh node is allways open
 		this.g = parrent.g + 1;		// the cost to get to a node is more the cost of the edge from the parrent plus the cost to get to the parrent
-		this.h = calculateH(goal);	// Calculate h for the node
+		this.h = calculateH(goalState);	// Calculate h for the node
 		this.visited = false;
 	}
 	
@@ -56,7 +56,7 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 	}
 
 	public double getF() {
-		return f;
+		return getG() + getH();
 	}
 
 	public double getG() {
@@ -82,15 +82,15 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 	public void addPossibleParrents(ArrayList<SearchNode> possParrent) {
 		this.possParrent.addAll(possParrent);
 		SearchNode bestParrent = possParrent.get(0);
-		for(int i=0;i<possParrent.size();i++) {
+		for(int i=0;i<possParrent.size();i++) {					// Finds the best parrent amongst the new parrents
 			if(possParrent.get(i).getG()>bestParrent.getG()) {
 				bestParrent = possParrent.get(i);
 			}
 		}
-		updateG(bestParrent.getG());
+		updateParrent(parrent);									// Updates the best parrent
 	}
 	
-	private double calculateH(SearchNode goal) {
+	private double calculateH(State goalstate) {
 		return 1;		// In here the code for the spesific problem you want to solve should be
 	}
 
@@ -103,16 +103,18 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 		return (int)((this.f - ((SearchNode)anotherNode).f)*ROUNDINGCONSTANT); //multiplying by a rounding constant to get more exact integer sorting
 	}
 
-	private void updateG(double g) {
+	private boolean updateParrent(SearchNode Parrent) {
 		// If the g is higher than the new g we update the g and all it's childrens g 	
-		if(this.g>g) {								
-			this.g=g;
+		if(this.g>Parrent.getG()) {								
+			this.g=Parrent.getG();
 			if(children!=null) { // This is in case the children have not been calculated
 				for(SearchNode child:children) {
-					child.updateG(g+getStepCost());
+					child.updateParrent(this);
 				}
 			}
+			return true;
 		}
+		return false;
 	}
 
 	private double getStepCost() {
