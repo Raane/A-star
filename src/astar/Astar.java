9d3b9f05609363bc7ebd86996bbf9291d40484fe
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 02:51:54 +0200

    Changing the search in closed nodes to a search in visited nodes.

--- src/astar/Astar.java
+++ src/astar/Astar.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Astar {
-	ArrayList<SearchNode> agenda;
-	SearchNode goal;
-	ArrayList<SearchNode> closedNodes;
+	private static final State GOALSTATE = null; // This is where the goalstate should be coded in
+
+	private SearchNode goal;
+	private ArrayList<SearchNode> agenda;
+	private ArrayList<SearchNode> closedNodes;
+	private ArrayList<SearchNode> visitedNodes;
+	
 	public Astar() {
 		agenda = new ArrayList<SearchNode>();
-		goal = new SearchNode(State goalState);
+		goal = new SearchNode(GOALSTATE);
 		closedNodes = new ArrayList<SearchNode>();
+		visitedNodes = new ArrayList<SearchNode>();
 	}
 	
 	public SearchNode run(SearchNode root) {
@@ -21,16 +26,20 @@ public class Astar {
 			if(currentNode.getState().equals(goal.getState())) {
 				return currentNode;
 			}
-			for(SearchNode closedNode:closedNodes) {
-				if(currentNode.equals(closedNode)) {
-					closedNode.updateG(currentNode.getG());
-					currentNode = closedNode;
-					break;
+			for(SearchNode closedNode:visitedNodes) {
+				if(currentNode.equals(closedNode) && !currentNode.equals(closedNode)) {	// Checks if there is another different node with the same state
+					closedNode.updateG(currentNode.getG());	// Lets the new identical node bubble up the graph
+					currentNode = closedNode;	// Sets the node we are working on to the new better and equal node
+					break;	// Exits as there should never be identical states in the nodes allready in the visited nodes
 				}
 			}
 			for(SearchNode child:currentNode.getChildren()) {				
 				agenda.add(child);
 			}
+			if(closedNodes.containsAll(currentNode.getChildren())) {
+				agenda.remove(currentNode);
+			}
+			visitedNodes.add(currentNode);
 		}
 		return null;
 	}
