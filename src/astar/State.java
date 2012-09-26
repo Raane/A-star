fb794610f7d1936eca69c1865a3fb22c8718f3f4
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 02:10:11 +0200

    Making the State comparable by equals

--- src/astar/State.java
+++ src/astar/State.java
@@ -1,5 +1,11 @@
 package astar;
 
+import java.util.Collections;
+
 public class State {
 	// This class have to be custom made for a spesific problem.
+	@Override
+	public boolean equals(Object arg0) {
+		return super.equals(arg0);		// This should return if the state is the same 
+	}
 }
