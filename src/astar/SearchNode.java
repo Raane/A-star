fae56f7b8e30163fe274203dbcc59bd956299e91
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 15:49:18 +0200

    Making the bubeling up the tree happen whenever a possparrent is added

--- src/astar/SearchNode.java
+++ src/astar/SearchNode.java
@@ -10,6 +10,7 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 	private double g;							// This is the estimated cost of getting from the node to the goal
 	private double h;							// This is the sum of f & g
 	private boolean open;						// This is the status (open/closed) of the node
+	private boolean visited;
 	private boolean solution;					// This indicates if the node is a solution
 	private SearchNode parrent;					// This indicates the best known parrent
 	private ArrayList<SearchNode> children;		// This is all of the explorableChildren of node
@@ -19,8 +20,17 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 		this.open = true;			// A fresh node is allways open
 		this.g = parrent.g + 1;		// the cost to get to a node is more the cost of the edge from the parrent plus the cost to get to the parrent
 		this.h = calculateH(goal);	// Calculate h for the node
+		this.visited = false;
 	}
 	
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
 	public SearchNode(State goal) {
 		this.state = goal;
 	}
@@ -69,11 +79,16 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 		return possParrent;
 	}
 
+	public void addPossibleParrent(SearchNode possParrent) {
+		this.possParrent.add(possParrent);
+		updateG(possParrent.getG());
+	}
+	
 	private double calculateH(SearchNode goal) {
 		return 1;		// In here the code for the spesific problem you want to solve should be
 	}
 
-
+	
 
 	@Override
 	public int compareTo(Object anotherNode) {
@@ -82,10 +97,7 @@ public class SearchNode implements Comparable{  // Implements comparable for sor
 		return (int)((this.f - ((SearchNode)anotherNode).f)*ROUNDINGCONSTANT); //multiplying by a rounding constant to get more exact integer sorting
 	}
 
-	public void updateG(double g) {
-		
-		//TODO This should probably happen when a new possibleParrent is added.
-		
+	private void updateG(double g) {
 		// If the g is higher than the new g we update the g and all it's childrens g 	
 		if(this.g>g) {								
 			this.g=g;
