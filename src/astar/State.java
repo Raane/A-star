db2f2be32eb4eb00f73203166fafdf203f1d3bda
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 18:03:01 +0200

    Initiated the State for ex1

--- src/astar/State.java
+++ src/astar/State.java
@@ -4,8 +4,29 @@ import java.util.Collections;
 
 public class State {
 	// This class have to be custom made for a spesific problem.
+	private static final int PROBLEMSIZE = 9;
+	private int[] stateArray = new int[PROBLEMSIZE];
+	
+	public State(int[] stateArray) {
+		this.stateArray = stateArray;
+	}
+	public State(State parrent, int a, int b) {
+		this.stateArray = stateArray;
+		int temp = stateArray[a];
+		stateArray[a] = stateArray[b];
+		stateArray[b] = temp;
+	}
+	
 	@Override
-	public boolean equals(Object arg0) {
-		return super.equals(arg0);		// This should return if the state is the same 
+	public boolean equals(Object anotherState) {
+		boolean equal = true;
+		for(int i=0;i<PROBLEMSIZE;i++) {
+			if(((State) anotherState).getStateArrayCell(i)==stateArray[i])
+		}
+		return equal;		// This should return if the state is the same 
+	}
+	private int getStateArrayCell(int cell) {
+		// TODO Auto-generated method stub
+		return stateArray[cell];
 	}
 }
