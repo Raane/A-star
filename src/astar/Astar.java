246cfe771a01bf77db9e48c69c71c1603f4e48a0
Rune Holmgren <Raane.Holm@gmail.com>
2012-09-26 02:22:01 +0200

    Making the agendaloop check if it's a goal state

--- src/astar/Astar.java
+++ src/astar/Astar.java
@@ -6,18 +6,25 @@ import java.util.Collections;
 public class Astar {
 	ArrayList<SearchNode> agenda;
 	SearchNode goal;
+	ArrayList<SearchNode> closedNodes;
 	public Astar() {
 		agenda = new ArrayList<SearchNode>();
 		goal = new SearchNode(State goalState);
+		closedNodes = new ArrayList<SearchNode>();
 	}
 	
-	public void run(SearchNode root) {
+	public SearchNode run(SearchNode root) {
 		agenda.add(root);
 		while(agenda.size()!=0) {
 			Collections.sort(agenda);			// Sorting the agenda
 			SearchNode currentNode = agenda.remove(0);		//
-			if(currentNode.getState().equals(goal.getState()));
-			agenda.
+			if(currentNode.getState().equals(goal.getState())) {
+				return currentNode;
+			}
+			for(SearchNode child:currentNode.getChildren()) {				
+				agenda.add(child);
+			}
 		}
+		return null;
 	}
 }
